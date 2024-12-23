6bcea3b6865f143b7d92c26c7a097ad05deb8f18
Mathieu <mathieu.dejob@gmail.com>
2024-12-23 17:00:52 +0100

    Uncomment RecipeLIst & Recipe component functionalities

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-// import { useState } from "react";
-// import DeleteButton from "./buttons/DeleteButton";
-// import EditButton from "./buttons/EditButton";
-// import EditRecipeModal from "./EditRecipeModal";
+import { useState } from "react";
+import DeleteButton from "./buttons/DeleteButton";
+import EditButton from "./buttons/EditButton";
+import EditRecipeModal from "./EditRecipeModal";
 
 export type RecipeType = {
   id: number;
@@ -14,33 +14,32 @@ export type RecipeType = {
 
 type RecipeProps = {
   recipe: RecipeType;
-  // onDelete: (id: number) => Promise<void>;
-  // onUpdate: (updatedRecipe: RecipeType) => void; // Callback for updating the recipe
+  onDelete: (id: number) => Promise<void>;
+  onUpdate: (updatedRecipe: RecipeType) => void; // Callback for updating the recipe
 };
 
-const Recipe: React.FC<RecipeProps> = ({ recipe }) => {
-  // const Recipe: React.FC<RecipeProps> = ({ recipe, onDelete, onUpdate }) => {
-  // const [loading, setLoading] = useState(false); // Local loading state
-  // const [isEditModalOpen, setEditModalOpen] = useState(false);
+const Recipe: React.FC<RecipeProps> = ({ recipe, onDelete, onUpdate }) => {
+  const [loading, setLoading] = useState(false); // Local loading state
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
 
-  // const handleDelete = async () => {
-  //   setLoading(true);
-  //   await onDelete(recipe.id); // Ensure Promise resolves before toggling loading
-  //   setLoading(false);
-  // };
+  const handleDelete = async () => {
+    setLoading(true);
+    await onDelete(recipe.id); // Ensure Promise resolves before toggling loading
+    setLoading(false);
+  };
 
-  // const handleEditClick = () => {
-  //   setEditModalOpen(true); // Open the modal
-  // };
+  const handleEditClick = () => {
+    setEditModalOpen(true); // Open the modal
+  };
 
-  // const handleModalClose = () => {
-  //   setEditModalOpen(false); // Close the modal
-  // };
+  const handleModalClose = () => {
+    setEditModalOpen(false); // Close the modal
+  };
 
-  // const handleModalSubmit = (updatedRecipe: RecipeType) => {
-  //   onUpdate(updatedRecipe); // Call the update function passed as a prop
-  //   handleModalClose(); // Close the modal after updating
-  // };
+  const handleModalSubmit = (updatedRecipe: RecipeType) => {
+    onUpdate(updatedRecipe); // Call the update function passed as a prop
+    handleModalClose(); // Close the modal after updating
+  };
 
   return (
     <div className="mb-2">
@@ -52,12 +51,20 @@ const Recipe: React.FC<RecipeProps> = ({ recipe }) => {
             Ingredients: {recipe.ingredients.join(", ")}
           </p>
         </div>
+        <div className="flex justify-between">
+          <EditButton onEdit={handleEditClick} loading={loading} />
+          <span className="w-2"></span>
+          <DeleteButton onDelete={handleDelete} loading={loading} />
+        </div>
       </li>
 
       {/* Render the EditRecipeModal */}
-      {/* <EditRecipeModal
+      <EditRecipeModal
+        isOpen={isEditModalOpen}
+        onClose={handleModalClose}
+        onSubmit={handleModalSubmit}
         recipe={recipe} // Pass the current recipe details to the modal
-      /> */}
+      />
     </div>
   );
 };
--- src/components/RecipesList.tsx
+++ src/components/RecipesList.tsx
@@ -2,132 +2,155 @@
 
 import React, { useEffect, useState } from "react";
 import Recipe, { RecipeType } from "./Recipe";
-// import AddRecipeButton from "./buttons/AddRecipeButton";
-// import RecipeFormModal from "./RecipeFormModal";
+import AddRecipeButton from "./buttons/AddRecipeButton";
+import RecipeFormModal from "./RecipeFormModal";
 
 const RecipeList: React.FC = () => {
   const [recipes, setRecipes] = useState<RecipeType[]>([]);
-  // const [loading, setLoading] = useState(false);
-  // const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    console.log("Use effect");
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes/");
+        if (!response.ok) {
+          throw new Error(`Failed to fetch: ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log("Fetched recipes:", data);
+        setRecipes(data);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      }
+    };
     fetchRecipes();
   }, []);
 
   const fetchRecipes = async () => {
-    // setLoading(true);
+    setLoading(true);
     try {
       const res = await fetch("/api/recipes");
       if (!res.ok) {
-        throw new Error("Failed to fetch recipes");
+        const errorDetails = await res.text(); // Attempt to read the response body for more details
+        throw new Error(
+          `Failed to fetch recipes. Status: ${res.status} - ${res.statusText}. Details: ${errorDetails}`
+        );
       }
       const data = await res.json();
       setRecipes(data);
     } catch (error) {
-      console.error("An error occurred:", error);
+      if (error instanceof Error) {
+        // Log a more detailed error message
+        console.error("An error occurred while fetching recipes:", {
+          message: error.message,
+          stack: error.stack,
+        });
+      } else {
+        // Handle non-Error objects, just in case
+        console.error("An unknown error occurred:", error);
+      }
+    } finally {
+      setLoading(false);
     }
-    // finally {
-    // setLoading(false);
-    // }
   };
 
-  // const handleDelete = async (id: number): Promise<void> => {
-  //   const res = await fetch("/api/recipes", {
-  //     method: "DELETE",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ id }),
-  //   });
+  const handleDelete = async (id: number): Promise<void> => {
+    const res = await fetch("/api/recipes", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id }),
+    });
 
-  //   if (res.ok) {
-  //     setRecipes((prevRecipes) =>
-  //       prevRecipes.filter((recipe) => recipe.id !== id)
-  //     );
-  //   } else {
-  //     console.error("Failed to delete recipe.");
-  //   }
-  // };
+    if (res.ok) {
+      setRecipes((prevRecipes) =>
+        prevRecipes.filter((recipe) => recipe.id !== id)
+      );
+    } else {
+      console.error("Failed to delete recipe.");
+    }
+  };
 
-  // const handleUpdate = async (updatedRecipe: RecipeType) => {
-  //   try {
-  //     console.log(updatedRecipe);
+  const handleUpdate = async (updatedRecipe: RecipeType) => {
+    try {
+      console.log(updatedRecipe);
 
-  //     const res = await fetch(`/api/recipes/${updatedRecipe.id}`, {
-  //       method: "PUT",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(updatedRecipe),
-  //     });
+      const res = await fetch(`/api/recipes/${updatedRecipe.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedRecipe),
+      });
 
-  //     if (res.ok) {
-  //       setRecipes((prevRecipes) =>
-  //         prevRecipes.map((recipe) =>
-  //           recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-  //         )
-  //       );
-  //     } else {
-  //       console.error("Failed to update recipe.");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   }
-  // };
+      if (res.ok) {
+        setRecipes((prevRecipes) =>
+          prevRecipes.map((recipe) =>
+            recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+          )
+        );
+      } else {
+        console.error("Failed to update recipe.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
 
-  // const handleAddRecipeClick = () => {
-  //   setIsModalOpen(true);
-  // };
+  const handleAddRecipeClick = () => {
+    setIsModalOpen(true);
+  };
 
-  // const handleAddRecipeSubmit = async (recipeData: {
-  //   title: string;
-  //   description: string;
-  //   ingredients: string[];
-  // }) => {
-  //   try {
-  //     const res = await fetch("/api/recipes", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(recipeData),
-  //     });
+  const handleAddRecipeSubmit = async (recipeData: {
+    title: string;
+    description: string;
+    ingredients: string[];
+  }) => {
+    try {
+      const res = await fetch("/api/recipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(recipeData),
+      });
 
-  //     if (res.ok) {
-  //       fetchRecipes(); // Refresh the recipe list after adding
-  //     } else {
-  //       console.error("Failed to add recipe.");
-  //     }
-  //   } catch (error) {
-  //     console.error("An error occurred:", error);
-  //   } finally {
-  //     setIsModalOpen(false); // Close the modal after submission
-  //   }
-  // };
+      if (res.ok) {
+        fetchRecipes(); // Refresh the recipe list after adding
+      } else {
+        console.error("Failed to add recipe.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    } finally {
+      setIsModalOpen(false); // Close the modal after submission
+    }
+  };
 
   return (
     <div className="p-4">
-      {/* <AddRecipeButton onAdd={handleAddRecipeClick} loading={loading} />
+      <AddRecipeButton onAdd={handleAddRecipeClick} loading={loading} />
       <RecipeFormModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         onSubmit={handleAddRecipeSubmit}
-      /> */}
-      {/* {loading ? (
+      />
+      {loading ? (
         <p className="text-center text-gray-500 mt-4">Loading recipes...</p>
-      ) : ( */}
-      <ul className="space-y-4 mt-4">
-        {recipes.map((recipe) => (
-          <Recipe
-            key={recipe.id}
-            recipe={recipe}
-            // onDelete={handleDelete}
-            // onUpdate={handleUpdate} // Pass the update handler to the Recipe component
-          />
-        ))}
-      </ul>
-      {/* )} */}
+      ) : (
+        <ul className="space-y-4 mt-4">
+          {recipes.map((recipe) => (
+            <Recipe
+              key={recipe.id}
+              recipe={recipe}
+              onDelete={handleDelete}
+              onUpdate={handleUpdate} // Pass the update handler to the Recipe component
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
