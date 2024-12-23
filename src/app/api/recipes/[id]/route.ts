ef840c356e002364a1f07afe2784d7054146a401
Mathieu <mathieu.dejob@gmail.com>
2024-12-23 19:11:06 +0100

    Update post edit route to match with nextjs 15

--- src/app/api/recipes/[id]/route.ts
+++ src/app/api/recipes/[id]/route.ts
@@ -3,21 +3,24 @@ import { prisma } from "@/lib/prisma";
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
-  const { title, description, ingredients } = await req.json();
+  const id = (await params).id;
 
   try {
+    const { title, description, ingredients } = await req.json();
+
     const updatedRecipe = await prisma.recipe.update({
       where: { id: parseInt(id) },
       data: { title, description, ingredients },
     });
+
     return NextResponse.json(updatedRecipe, { status: 200 });
-    // } catch (error) {
   } catch (error) {
+    console.error("Error updating recipe:", error);
+
     return NextResponse.json(
-      { error: "Failed to update recipe" + error },
+      { error: "Failed to update recipe" },
       { status: 500 }
     );
   }
